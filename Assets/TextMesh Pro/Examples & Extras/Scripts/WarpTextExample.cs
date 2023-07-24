62993f5e7f9f6316309c33528030a5869f32d204
Shalin06 <jain.75@iitj.ac.in>
2023-07-24 19:36:13 +0530

    update

--- Assets/TextMesh Pro/Examples & Extras/Scripts/WarpTextExample.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/WarpTextExample.cs
@@ -40,7 +40,7 @@ namespace TMPro.Examples
         /// <summary>
         ///  Method to curve text along a Unity animation curve.
         /// </summary>
-        /// <param uname="textComponent"></param>
+        /// <param name="textComponent"></param>
         /// <returns></returns>
         IEnumerator WarpText()
         {
